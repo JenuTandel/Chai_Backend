4d911aae26084b4e8607d4afaf4bf6699511ee5e
JinalTandel <jinal.tandel@1rivet.com>
2025-04-02 19:36:04 +0530

    feat: Configuared all setup
    Configuared all setups

--- src/app.js
+++ src/app.js
@@ -1,24 +1,37 @@
-import cors from 'cors';
-import express from 'express';
+import cors from "cors";
+import express from "express";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
+    credentials: true,
+  }),
+);
 
 // for json file limit
-app.use(express.json({
-    limit: '20kb'
-}));
+app.use(
+  express.json({
+    limit: "20kb",
+  }),
+);
 
 // For url config
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true,
-    limit:'20kb'
-}));
+    limit: "20kb",
+  }),
+);
 
 // For static files to store assets
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
 export default app;
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,3 @@
-export const asyncHandler = (requestHandler) => (req, res, next) =>{
-    Promise.resolve(requestHandler(req, res, next)).catch((err)=>next(err));
-}
+export const asyncHandler = (requestHandler) => (req, res, next) => {
+  Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+};
